278555589c79f18c1fe7a68ee2152edd25dbc1ef
Saad Guessous <guessous_saad@hotmail.com>
2021-04-07 19:20:23 +0300

    feat: add semantic-release workflow

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/sguessou/react-app-github-actions",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
-    ]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/sguessou/react-app-github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
